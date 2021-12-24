d793635a11181fa448ec5531b1346484fea95d87
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-12-23 22:34:12 -0800

    Improve tests

--- tests/clusterer_test.py
+++ tests/clusterer_test.py
@@ -2,22 +2,26 @@ import unittest
 
 from src.text_based_clusterer.clusterer import Clusterer
 
+
 class StringContainer:
     def __init__(self, str_1: str, str_2: str):
         self.str_1 = str_1
         self.str_2 = str_2
 
+    def __repr__(self):
+        return '"' + self.str_1 + ' ' + self.str_2 + '"'
+
+
 class ClustererTest(unittest.TestCase):
     CLUSTER_THRESHOLD = 0.3
-    ACTIVE_THRESHOLD = 60
 
     def test_single_attribute(self):
-        print('Testing clustering using one attributes:')
+        print('Testing clustering using one attribute:')
 
         string_container_1 = StringContainer('dog', 'cat')
-        string_container_2 = StringContainer('dog', 'cat')
-        string_container_3 = StringContainer('cat', 'dog')
-        string_container_4 = StringContainer('cat', 'dog')
+        string_container_2 = StringContainer('cat', 'dog')
+        string_container_3 = StringContainer('goat', 'sheep')
+        string_container_4 = StringContainer('goat', 'goat')
 
         string_containers = [string_container_1, string_container_2,
                              string_container_3, string_container_4]
@@ -29,12 +33,12 @@ class ClustererTest(unittest.TestCase):
         print(clusterer.get_clusters())
 
     def test_multiple_attributes(self):
-        print('Testing clustering using multiple attributes:')
+        print('Testing clustering using two attributes:')
 
         string_container_1 = StringContainer('dog', 'cat')
-        string_container_2 = StringContainer('dog', 'cat')
-        string_container_3 = StringContainer('cat', 'dog')
-        string_container_4 = StringContainer('cat', 'dog')
+        string_container_2 = StringContainer('cat', 'dog')
+        string_container_3 = StringContainer('goat', 'sheep')
+        string_container_4 = StringContainer('goat', 'goat')
 
         string_containers = [string_container_1, string_container_2,
                              string_container_3, string_container_4]
