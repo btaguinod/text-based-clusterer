d1be4fea073ff7f9c328768a8cb4b5944f937309
Benedict Taguinod <benedict.taguinod@berkeley.edu>
2021-12-23 22:34:54 -0800

    Genericise clustering algorithm

--- src/text_based_clusterer/clusterer.py
+++ src/text_based_clusterer/clusterer.py
@@ -4,39 +4,35 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import string
 import math
-from datetime import datetime
-
-from article import Article
-from event import Event
 
 nltk.download('stopwords')
 nltk.download('punkt')
 
 
-class NLPArticle:
-    """Article Container with vector for similarity comparisons.
+class NLPObject:
+    """Object Container with vector for similarity comparisons.
 
     Attributes:
-        article (Article): Article object.
-        vector (numpy.ndarray): Numpy vector representation.
+        object: Contained object.
+        vector: Numpy vector representation.
     """
 
-    def __init__(self, article: Article):
-        self.article = article
+    def __init__(self, object_arg: object):
+        self.object = object_arg
         self.vector = None
 
-    def set_tf_vector(self, index: list[str]) -> list[str]:
+    def set_tf_vector(self, index: list[str], attributes: list[str]) -> list[str]:
         """Calculate and store term frequency vector.
 
         Args:
-            index (list[str]): Word index.
+            index: Word index.
+            attributes: Names of object attributes.
 
         Returns:
-            list[str]: Updated word index.
+            Updated word index.
         """
-
         new_index = index.copy()
-        tokens = self.preprocess_text()
+        tokens = self.preprocess_text(attributes)
         unique_words = set(tokens)
 
         text_freq_dict = {word: 0 for word in unique_words}
@@ -60,19 +56,19 @@ class NLPArticle:
         """Pad the term frequency vector with zeros.
 
         Args:
-            padding_len (int): Length vector will be extended by.
+            padding_len: Length vector will be extended by.
         """
-
         self.vector = np.pad(self.vector, (0, padding_len))
 
-    def preprocess_text(self) -> list[str]:
+    def preprocess_text(self, attributes: list[str]) -> list[str]:
         """Turn text into tokens without stopwords and punctuation.
 
         Returns:
-            list[str]: List of tokens.
+            List of tokens.
         """
-
-        text = self.article.title + ' ' + self.article.description
+        text = ''
+        for attribute in attributes:
+            text += getattr(self.object, attribute) + ' '
         text = text.translate(str.maketrans('', '', string.punctuation))
         stop_words = set(stopwords.words('english'))
         tokens = word_tokenize(text)
@@ -84,148 +80,114 @@ class NLPArticle:
 
 
 class Cluster:
-    """Cluster of NLPArticle objects.
+    """Cluster of NLPObject objects.
 
     Attributes:
-        nlp_articles (list[NLPArticle]): Stored NLPArticle objects
-        self.vector (numpy.ndarray): Average of term frequency vectors in
-            NLPArticle objects.
+        nlp_objects: Stored NLPObject objects
+        self.vector: Average of term frequency vectors in NLPObject objects.
     """
 
-    def __init__(self, nlp_articles: list[NLPArticle], event_id: str = None):
-        self.nlp_articles = nlp_articles
+    def __init__(self, nlp_objects: list[NLPObject]):
+        self.nlp_objects = nlp_objects
         self.vector = None
-        self.event_id = event_id
 
     def set_tf_vector(self, index: list[str]):
         """Calculate and store average term frequency vector.
 
         Args:
-            index (list[str]): Word index.
+            index: Word index.
         """
-
         new_index = index
         self.vector = np.zeros(len(new_index))
-        for nlp_article in self.nlp_articles:
-            self.vector += nlp_article.vector
-        self.vector /= len(self.nlp_articles)
+        for nlp_object in self.nlp_objects:
+            self.vector += nlp_object.vector
+        self.vector /= len(self.nlp_objects)
 
     def pad_tf_vector(self, padding_len: int):
         """Pad the term frequency vector with zeros.
 
         Args:
-            padding_len (int): Length vector will be extended by.
+            padding_len: Length vector will be extended by.
         """
-
         self.vector = np.pad(self.vector, (0, padding_len))
 
-    def get_event(self) -> Event:
-        """Gets event representation.
+    def get_list(self) -> list[object]:
+        """Gets list representation.
 
         Returns:
-            Event: Event representation.
+            List representation.
         """
-
-        articles = []
-        for nlp_article in self.nlp_articles:
-            articles.append(nlp_article.article)
-        return Event(articles, self.event_id)
+        objects = []
+        for nlp_object in self.nlp_objects:
+            objects.append(nlp_object.object)
+        return objects
 
 
 class Clusterer:
-    """Clusters Articles into Events.
+    """Clusters objects.
 
     Attributes:
-        cluster_threshold (float): Cosine similarity from 0 to 1 for clustering
+        cluster_threshold: Cosine similarity from 0 to 1 for clustering
             strictness.
-        active_threshold (int): Number of days until old events are inactive.
-        clusters (list[Cluster]): Stored Cluster objects.
-        inactive_events (list[Events]): Event objects that won't be
-            considered in clustering.
-        index (list[str]): Word index.
+        clusters: Stored Cluster objects.
+        index: Word index.
     """
 
-    def __init__(self, cluster_threshold: float = 0.3,
-                 active_threshold: int = 2):
+    def __init__(self, cluster_threshold: float = 0.3):
         self.cluster_threshold = cluster_threshold
-        self.active_threshold = active_threshold
         self.clusters = []
-        self.inactive_events = []
         self.index = []
 
-    def add_events(self, events: list[Event]):
-        """Set event objects as clusters and label old events inactive.
+    def add_clusters(self, cluster_lists: list[list[object]]):
+        """Add existing clusters.
 
         Args:
-            events (list[Event]): Article objects.
+            cluster_lists: Clustered lists of objects.
         """
-
-        today = datetime.today()
-        for event in events:
-            nlp_articles = []
-            for article in event.articles:
-                nlp_articles.append(NLPArticle(article))
-            self.clusters.append(Cluster(nlp_articles, event.event_id))
+        for cluster_list in cluster_lists:
+            nlp_objects = []
+            for cluster_object in cluster_list:
+                nlp_objects.append(NLPObject(cluster_object))
+            self.clusters.append(Cluster(nlp_objects))
 
         for cluster in self.clusters:
-            for nlp_article in cluster.nlp_articles:
-                self.index = nlp_article.set_tf_vector(self.index)
+            for nlp_object in cluster.nlp_objects:
+                self.index = nlp_object.set_tf_vector(self.index)
             index_len = len(self.index)
-            for nlp_article in cluster.nlp_articles:
-                padding_len = index_len - len(nlp_article.vector)
-                nlp_article.pad_tf_vector(padding_len)
+            for nlp_object in cluster.nlp_objects:
+                padding_len = index_len - len(nlp_object.vector)
+                nlp_object.pad_tf_vector(padding_len)
             cluster.set_tf_vector(self.index)
 
-    def add_articles(self, articles: list[Article]):
-        """Add Article objects to cluster.
+    def add_objects(self, objects: list[object], attributes: list[string]):
+        """Add objects to clusters.
 
         Args:
-            articles (list[Article]): Article objects.
+            objects: Objects to cluster.
+            attributes: List of strings corresponding to attributes.
         """
-
-        articles = sorted(articles, key=lambda x: x.published_time)
-        nlp_articles = [NLPArticle(article) for article in articles]
-        for nlp_article in nlp_articles:
-            self.index = nlp_article.set_tf_vector(self.index)
+        nlp_objects = [NLPObject(object_item) for object_item in objects]
+        for nlp_object in nlp_objects:
+            self.index = nlp_object.set_tf_vector(self.index, attributes)
 
         index_len = len(self.index)
-        for nlp_article in nlp_articles:
-            padding_len = index_len - len(nlp_article.vector)
-            nlp_article.pad_tf_vector(padding_len)
+        for nlp_object in nlp_objects:
+            padding_len = index_len - len(nlp_object.vector)
+            nlp_object.pad_tf_vector(padding_len)
         for cluster in self.clusters:
             padding_len = index_len - len(cluster.vector)
-            for cluster_nlp_article in cluster.nlp_articles:
-                cluster_nlp_article.pad_tf_vector(padding_len)
+            for cluster_nlp_object in cluster.nlp_objects:
+                cluster_nlp_object.pad_tf_vector(padding_len)
             cluster.pad_tf_vector(padding_len)
 
-        inv_doc_freq = self.get_inv_doc_freq(nlp_articles)
-        for nlp_article in nlp_articles:
+        inv_doc_freq = self.get_inv_doc_freq(nlp_objects)
+        for nlp_object in nlp_objects:
             closest_dist = 0
             closest_cluster = None
 
             for cluster in self.clusters.copy():
-                article_time = datetime.strptime(
-                    nlp_article.article.published_time,
-                    '%Y-%m-%dT%H:%M:%SZ'
-                )
-                cluster_article = max(
-                    cluster.nlp_articles,
-                    key=lambda x: x.article.published_time
-                ).article
-                cluster_article_time = datetime.strptime(
-                    cluster_article.published_time,
-                    '%Y-%m-%dT%H:%M:%SZ'
-                )
-                days_difference = (article_time - cluster_article_time).days
-                if days_difference > self.active_threshold:
-                    event = cluster.get_event()
-                    event.active = False
-                    self.inactive_events.append(event)
-                    self.clusters.remove(cluster)
-                    continue
-
                 new_dist = vector_similarity(
-                    np.multiply(nlp_article.vector, inv_doc_freq),
+                    np.multiply(nlp_object.vector, inv_doc_freq),
                     np.multiply(cluster.vector, inv_doc_freq))
 
                 if new_dist > self.cluster_threshold and new_dist > closest_dist:
@@ -233,43 +195,39 @@ class Clusterer:
                     closest_cluster = cluster
 
             if closest_cluster is None:
-                new_cluster = Cluster([nlp_article])
+                new_cluster = Cluster([nlp_object])
                 self.clusters.append(new_cluster)
                 new_cluster.set_tf_vector(self.index)
             else:
-                closest_cluster.nlp_articles.append(nlp_article)
+                closest_cluster.nlp_objects.append(nlp_object)
                 closest_cluster.set_tf_vector(self.index)
 
-    def get_events(self) -> list[Event]:
-        """Get Event representation.
+    def get_clusters(self) -> list[list[object]]:
+        """Get clustered lists of objects.
 
         Returns:
-            list[Event]: Event representation.
+            clustered lists of objects.
         """
+        return [cluster.get_list() for cluster in self.clusters]
 
-        active_events = [cluster.get_event() for cluster in self.clusters]
-        return active_events + self.inactive_events
-
-    def get_inv_doc_freq(self, nlp_articles: list[NLPArticle]) -> np.ndarray:
+    def get_inv_doc_freq(self, nlp_objects: list[NLPObject]) -> np.ndarray:
         """Calculate inverse document frequency.
 
         Args:
-            nlp_articles (list[NLPArticle]): Extra NLPArticles to use in
-                calculations.
+            nlp_objects: Extra NLPObjects to use in calculations.
         """
-
         vector_len = len(self.index)
 
         doc_freq = np.zeros(vector_len)
-        for nlp_article in nlp_articles:
-            doc_freq += np.ceil(nlp_article.vector)
+        for nlp_object in nlp_objects:
+            doc_freq += np.ceil(nlp_object.vector)
         for cluster in self.clusters:
-            for nlp_article in cluster.nlp_articles:
-                doc_freq += np.ceil(nlp_article.vector)
+            for nlp_object in cluster.nlp_objects:
+                doc_freq += np.ceil(nlp_object.vector)
 
-        doc_count = len(nlp_articles)
+        doc_count = len(nlp_objects)
         for cluster in self.clusters:
-            doc_count += len(cluster.nlp_articles)
+            doc_count += len(cluster.nlp_objects)
 
         inv_doc_freq = np.zeros(vector_len)
         for i in range(len(doc_freq)):
@@ -285,11 +243,10 @@ def vector_similarity(a: np.ndarray, b: np.ndarray) -> float:
     """Calculate cosine similarity between vectors.
 
     Args:
-       a (numpy.ndarray): First numpy vector.
-       b (numpy.ndarray): Second numpy vector.
+       a: First numpy vector.
+       b: Second numpy vector.
 
     Returns:
         float: Cosine similarity.
     """
-
     return (a @ b) / (np.linalg.norm(a) * np.linalg.norm(b))
